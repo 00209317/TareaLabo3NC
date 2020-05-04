9275b8e2b6da8ea2f95fb8d45cf8c659d2771e7b
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-04 12:25:46 -0600

    Product ready

--- capasTarea3/src/main/java/com/uca/capas/domain/Product.java
+++ capasTarea3/src/main/java/com/uca/capas/domain/Product.java
@@ -4,7 +4,34 @@ public class Product {
 
 	Integer id;
 	String nombre;
-	String cantidad;
+	Integer cantidad;
+	public Product() {
+
+	}
+	public Product(Integer id, String nombre, Integer cantidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
 	
 	
 	
